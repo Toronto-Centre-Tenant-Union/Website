f5e8d8b871eafce570c2e51408aa009c2908d3c4
dstingaciu <dsstingaciu@gmail.com>
2024-03-02 21:56:17 -0500

    Added sitemap and robots

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Toronto Centre Tenant Union</title>
+        <meta name="robots" content="all" />
+        <meta
+          name="description"
+          content="The official website for the Toronto Centre Tenant Union Organization. Help us make Toronto a better place to live together!"
+        />
       </head>
       <body>
         <Providers>{children}</Providers>

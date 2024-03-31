55e106380999f151a6127575fd7002de731b236c
Dan Stingaciu <stingaciu.sebastian@gmail.com>
2024-03-30 23:42:49 -0400

    Added log messages

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -4,6 +4,8 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const calendarId = process.env.GOOGLE_CAL_ID || '';
   const apiKey = process.env.GOOGLE_API_KEY || '';
+  if(apiKey) console.log("FOUND API KEY")
+  if(calendarId) console.log("FOUND CALENDAR ID")
   const fetcher = await fetch(`https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(calendarId)}/events?key=${apiKey}`, {cache: 'no-cache'});
   const calendar = await fetcher.json();
   const events = calendar.items as GoogleCalendarEvent[]; // You might want to filter or process events further
